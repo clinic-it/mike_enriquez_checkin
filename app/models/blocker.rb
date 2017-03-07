ef86151186a3b18352a5224278cdfc91fabcabd8
Julius Ceasar Blanco <jcblanco@clinic-it.com>
2017-03-07 15:49:24 +0800

    Update associations

--- app/models/blocker.rb
+++ app/models/blocker.rb
@@ -1,2 +1,6 @@
 class Blocker < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :checkin
+  
 end
--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,6 +1,2 @@
 class Checkin < ActiveRecord::Base
-
-  belongs_to :user
-  belongs_to :project
-  
 end
