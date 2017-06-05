e1d7b4d9ab127a22f40eecda63eab2c49f45fdb1
Julius Ceasar Blanco <jcblanco@clinic-it.com>
2017-06-05 19:54:11 +0800

    Remove unused code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,14 +38,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['bucketname'],
-      :access_key_id => ENV['access_key_id'],
-      :secret_access_key => ENV['secret_access_key'],
-      :s3_region => ENV['region']
-    }
-  }
 end
