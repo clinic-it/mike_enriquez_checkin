7de09ac379349255c750c63ab45198ae2131bbee
Julius Ceasar Blanco <jcblanco@clinic-it.com>
2017-06-08 13:57:54 +0800

    Add summary checkin

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -8,4 +8,15 @@ class SummaryController < ApplicationController
       {:name => 'Today', :data => @today.users_with_checkin.map{|user| [user.username, user.current_load]} }
     ]
   end
+
+  def summary_checkin
+    @checkin = Checkin.last
+    respond_to do |format|
+      format.html
+      format.jpg do
+        kit = IMGKit.new render_to_string
+        send_data(kit.to_jpg, :type => "image/jpeg", :disposition => 'inline')
+      end
+    end
+  end
 end
