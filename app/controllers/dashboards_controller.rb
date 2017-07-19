78a1ec8afa246628edddc90d4afadb1dc58bed09
Julius Ceasar Blanco <jcblanco@clinic-it.com>
2017-07-19 13:03:01 +0800

    Refactor dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,26 +1,30 @@
 class DashboardsController < ApplicationController
 
+  before_action :init, :only => [:index]
+
+  
   def index
-    @checkin = CheckinForm.new Checkin.new
-    user = User.find_by_id current_user
-    pivotal = TrackerApi::Client.new :token =>  user.pivotal_token
-
-    @project_hash = {}
-
-    pivotal.projects.each do |project|
+    @pivotal.projects.each do |project|
       @project_hash[project.name] = []
 
-      project.iterations(:scope => 'done', :offset => -1).each do |iteration|
-        @project_hash[project.name].push iteration.stories.map{|story| {:project_id => story.project_id, :title => story.name, :url => story.url, :current_state => story.current_state, :estimate => story.estimate, :task_type => story.story_type, :task_id => story.id}}
-      end
-
-      project.iterations(:scope => 'current_backlog').each do |iteration|
+      [project.iterations(:scope => 'done', :offset => -1), project.iterations(:scope => 'current_backlog')].flatten.each do |iteration|
         @project_hash[project.name].push iteration.stories.map{|story| {:project_id => story.project_id, :title => story.name, :url => story.url, :current_state => story.current_state, :estimate => story.estimate, :task_type => story.story_type, :task_id => story.id}}
       end
 
       @project_hash[project.name].flatten!
     end
+  end
 
+
+
+  private
+
+  def init
+    @checkin = CheckinForm.new Checkin.new
+    user = User.find_by_id current_user
+    @pivotal = TrackerApi::Client.new :token =>  user.pivotal_token
+
+    @project_hash = {}
   end
 
 end
