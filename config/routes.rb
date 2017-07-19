9a335ad039390722953b0ba660669030e7f81199
Julius Ceasar Blanco <jcblanco@clinic-it.com>
2017-07-19 15:56:31 +0800

    Add users index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   scope '/api' do
     scope :module => :v1, :defaults => {:format => 'json'} do
       resources :projects, :only => [:index]
+      resources :users, :only => [:index]
     end
   end
 end
