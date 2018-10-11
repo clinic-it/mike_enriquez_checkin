0d7a4ad01213fe7dc4db460f4efc12e716f7c710
David Angulo <hello@davidangulo.xyz>
2018-10-11 13:35:10 +0800

    Move inline javascript to external javascript file.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,4 +7,16 @@ $(document).on('turbolinks:load', function() {
       }
     ]
   });
+
+  $('.js-image-preview').change(function() {
+    if ( this.files && this.files[0] ) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $('.js-image-holder').attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
 });
