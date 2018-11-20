f0f93087eeab1cc31fc4262774e46e37a7de5d35
David Angulo <hello@davidangulo.xyz>
2018-11-20 13:18:32 +0800

    Add smtp config on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {
+    :host => 'mikeenriquezcheckin.herokuapp.com'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',
+    :authentication => :login,
+    :user_name => ENV['gmail_email'],
+    :password => ENV['gmail_password']
+  }
 end
